9cc3cd0b9c7708fcf8b088c0e23fe56ff1abd831
Alex <zall99971@gmail.com>
2021-07-30 19:42:03 +0300

    Home work 4

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,13 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+//1. Создать окно для клиентской части чата: большое текстовое поле для отображения переписки в центре окна.
+// Однострочное текстовое поле для ввода сообщений и кнопка для отсылки сообщений на нижней панели.
+// Сообщение должно отсылаться либо по нажатию кнопки на форме, либо по нажатию кнопки Enter. При «отсылке» сообщение перекидывается из нижнего поля в центральное.
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("WebChat");
+        primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
 
